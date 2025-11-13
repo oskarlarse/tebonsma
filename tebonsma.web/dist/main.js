2e871dbdd2e7d6c8ce2bc0c1db4ad6531afd815d
Oskar Larsen <oskarla@stud.ntnu.no>
2025-11-13 14:20:48 +0100

    chore: update package.json to include Tailwind CSS and Vite plugin; remove unused main.ts file

--- tebonsma.web/dist/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const appDiv = document.getElementById('app');
-if (appDiv) {
-    appDiv.textContent = 'Hello from TypeScript!';
-}
-console.log('TypeScript is running!');
-//# sourceMappingURL=main.js.map
--- tebonsma.web/src/main.ts
+++
@@ -1,5 +0,0 @@
-    const appDiv = document.getElementById('app');
-    if (appDiv) {
-        appDiv.textContent = 'Hello from TypeScript!';
-    }
-    console.log('TypeScript is running!');
